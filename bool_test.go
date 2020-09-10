ca3e86a5fe6e2f997752eef7ee60ecfdbd739cac
Emmanuel Ay <emmanuel@block21.com>
2020-09-10 22:28:53 +0200

    Added tests for boolean type

---
+++ bool_test.go
@@ -0,0 +1,32 @@
+package jsontype
+
+import (
+	"testing"
+)
+
+func TestNotSetBool(t *testing.T) {
+	input := `{}`
+
+	p1 := parsePayload(input)
+	if (p1.IsActive.Set != false) || (p1.IsActive.Valid != false) {
+		t.FailNow()
+	}
+}
+
+func TestSetNullBool(t *testing.T) {
+	input := `{"active": null}`
+
+	p1 := parsePayload(input)
+	if (p1.IsActive.Set != true) || (p1.IsActive.Valid != false) {
+		t.FailNow()
+	}
+}
+
+func TestSetValidBool(t *testing.T) {
+	input := `{"active": true}`
+
+	p1 := parsePayload(input)
+	if (p1.IsActive.Set != true) || (p1.IsActive.Valid != true) || (p1.IsActive.Value != true) {
+		t.FailNow()
+	}
+}
